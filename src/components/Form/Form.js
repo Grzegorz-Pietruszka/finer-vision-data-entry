4a33f61fc02f511870cb2807cccd7b2c0293a441
GrzegorzP <pietruszka.grzegorz@gmail.com>
2020-10-24 21:28:21 +0100

    updated Form comp, added validateInput func

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import Accordion from "../Accordion/Accordion";
+import useFormValidation from "../helpers/useFormValidation";
+import validateInput from "../helpers/validateInput";
+
+const INITIAL_STATE = {
+    firstName: "",
+    surname: "",
+    emailAddress: "",
+    mobileNumber: ""
+};
+
 
 const Form = () => {
+
+    const {values, handleChange, handleBlur, errors} = useFormValidation(INITIAL_STATE, validateInput)
+
     return (
-        <Form>
-            form
-        </Form>
-    );
+        <Accordion>
+            <label>First Name
+                <input
+                    type='text'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    name='firstName'
+                    value={values.firstName}
+                />
+                {errors.email && <p>{errors.email}</p>}
+            </label>
+            <label>Surname
+                <input type='text'
+                       onChange={handleChange}
+                       name='surname'
+                       value={values.surname}
+                />
+            </label>
+            <label>Email Address
+                <input type='text'
+                       onChange={handleChange}
+                       name='emailAddress'
+                       value={values.emailAddress}/>
+            </label>
+            <label>Mobile Number
+                <input type='text'
+                       onChange={handleChange}
+                       name='mobileNumber'
+                       value={values.mobileNumber}
+                />
+            </label>
+
+            {/*gender*/}
+            {/*date of birth*/}
+            {/*comments*/}
+        </Accordion>
+    )
+        ;
 };
 
 export default Form;
