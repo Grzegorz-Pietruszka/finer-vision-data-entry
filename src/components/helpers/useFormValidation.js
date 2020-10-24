2c123323a3911de0791cc8a3c4ea40e16054e9c5
GrzegorzP <pietruszka.grzegorz@gmail.com>
2020-10-24 21:27:41 +0100

    added handleBlur to useFromValidation

--- src/components/helpers/useFormValidation.js
+++ src/components/helpers/useFormValidation.js
@@ -2,13 +2,22 @@ import React from "react";
 
 const useFormValidation = (initialState, validate) => {
     const [values, setValues] = React.useState(initialState);
+    const [errors, setErrors] = React.useState({});
 
     function handleChange(event) {
         setValues({
             ...values,
             [event.target.name]: event.target.value
         });
+        console.log(values)
     }
+
+    function handleBlur() {
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+    }
+
+    return {values, handleChange, handleBlur, errors}
 };
 
 export default useFormValidation;
